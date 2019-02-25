5fcba2c60d488ad1df37a84ff2dbb346ee10a13e
fulong <fulong@buyercamp.com>
2019-02-25 17:00:54 +0800

    sql

--- localhost.sql
+++ localhost.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: 2019-02-25 07:37:11
+-- Generation Time: 2019-02-25 17:00:06
 -- 服务器版本： 5.7.21
--- PHP Version: 5.6.33
+-- PHP Version: 7.2.7
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -33,8 +33,8 @@ CREATE TABLE `user` (
   `name` varchar(255) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL,
   `email` varchar(255) DEFAULT NULL,
-  `is_admin` int(2) NOT NULL DEFAULT '0',
-  `status` int(2) NOT NULL,
+  `is_admin` int(2) DEFAULT '0',
+  `status` int(2) DEFAULT '1',
   `create_time` int(10) DEFAULT NULL,
   `update_time` int(10) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -44,7 +44,13 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `name`, `password`, `email`, `is_admin`, `status`, `create_time`, `update_time`) VALUES
-(121, '123456', '7c4a8d09ca3762af61e59520943dc26494f8941b', '123452@qq.com', 0, 0, 1550855006, 1550855006);
+(121, '123456', '7c4a8d09ca3762af61e59520943dc26494f8941b', '123452@qq.com', 0, 0, 1550855006, 1550855006),
+(152, '寇伏龙', '7c4a8d09ca3762af61e59520943dc26494f8941b', '111111@qq.com', 1, 1, NULL, NULL),
+(153, '在线考试', '7c4a8d09ca3762af61e59520943dc26494f8941b', '222222@qq.com', NULL, 1, NULL, NULL),
+(154, '叶绿素', '7c4a8d09ca3762af61e59520943dc26494f8941b', '333333@qq.com', NULL, 1, NULL, NULL),
+(155, '易立购1', '9d61ba84065fc83956cdfc63e49bc7a9d21d8665', '123@qq.com', NULL, 1, NULL, NULL),
+(160, '易立购1', '7c4a8d09ca3762af61e59520943dc26494f8941b', '1111211@qq.com', 0, 1, NULL, NULL),
+(161, 'ascvs', '7c4a8d09ca3762af61e59520943dc26494f8941b', '5631386572@qq.com', 0, 1, NULL, NULL);
 
 --
 -- Indexes for dumped tables
@@ -64,7 +70,7 @@ ALTER TABLE `user`
 -- 使用表AUTO_INCREMENT `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=122;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=162;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
